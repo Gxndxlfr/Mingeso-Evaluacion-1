7517f00ae30b4a9b78eade9696499d14fafda23d
israel <israel.guzman@usach.cl>
2022-09-13 16:12:34 -0300

    conección BD + clase empleado

---
+++ evaluacion1/src/main/java/com/example/evaluacion1/entities/EmpleadoEntity.java
@@ -0,0 +1,26 @@
+package com.example.evaluacion1.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "empleados")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmpleadoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+
+    private String rut;
+    private String nombre;
+    private double sueldoMensual;
+    private int numeroHijos;
+    private String categoria;
+}
